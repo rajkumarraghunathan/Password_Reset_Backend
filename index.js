7b503bd122e5cdfcda727c5f12616595afcec0e8
Rajkumar <114281193+rajkumarraghunathan@users.noreply.github.com>
2023-07-25 18:47:58 +0530

    Latest commit

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ dotenv.config();
 app.use(express.json());
 app.use(cors(
     {
-        origin: 'http://localhost:3000',
+        origin: 'https://cozy-starship-234322.netlify.app',
         credentials: true,
     }
 ))
